73772a7e263e9e6e71856c6376aee6094ed6cea6
Ajeet Shah <ajeets.shah@hotmail.com>
2021-03-17 11:10:54 +0530

    git init

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import styles from "./App.module.css";
+
+export default function App() {
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <div className={styles.Hero}></div>
+    </div>
+  );
+}
